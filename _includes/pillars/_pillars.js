bc07144dd1a28c1e385695b766b82f4e44a7d274
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2024-07-19 14:47:29 -0400

    pillars redesign and js for full width

---
+++ _includes/pillars/_pillars.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const pillar = document.getElementById('nysds-pillar-id');
+    const container = pillar.closest(".o-wysiwyg");
+    container.style.maxWidth = '100%';
+})
