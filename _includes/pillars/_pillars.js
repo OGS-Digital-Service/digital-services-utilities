d9c5b118d582aef477ef21697ca7485879c44015
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2024-07-19 15:48:57 -0400

    style new pillar card areas

--- _includes/pillars/_pillars.js
+++ _includes/pillars/_pillars.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     const pillar = document.getElementById('nysds-pillar-id');
     const container = pillar.closest(".o-wysiwyg");
     container.style.maxWidth = '100%';
+    container.style.padding = "0px";
 })
