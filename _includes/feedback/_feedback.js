1d91b238ad07aede232f64a89ee6bc72d03cb914
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2024-10-28 15:44:20 -0400

    finalize url passing for feedback
    detect window location
    append to correct url
    retain everything else

--- _includes/feedback/_feedback.js
+++ _includes/feedback/_feedback.js
@@ -2,10 +2,14 @@ const feedbackText = document.getElementById('nysds-feedback-helpful');
 const feedbackArea = document.getElementById('nysds-feedback-options-area');
 const feedbackLink = document.getElementsByClassName('nysds-feedback-link');
 const linkArr = Array.from(feedbackLink);
-
-
-var referrerURL = window.location;
-    console.log(referrerURL)
+const referrerURL = window.location.href;
+const thumbsDown = document.getElementById('nysds-feedback-no');
+const thumbsUp = document.getElementById('nysds-feedback-yes');
+const feedbackForm = "https://forms.ny.gov/s3/Thank-you-for-your-Feedback"
+const fragmentUp = "?thumbs=Up?referrerUrl="
+const fragmentDown = "?thumbs=Down?referrerUrl="
+const urlUp = feedbackForm + fragmentUp + referrerURL
+const urlDown = feedbackForm + fragmentDown + referrerURL
 
 for (let i = 0; i < linkArr.length; i++) {
     linkArr[i].addEventListener("click", feedbackChange) // change area on click
@@ -15,16 +19,19 @@ for (let i = 0; i < linkArr.length; i++) {
         }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const feedback = document.getElementById('nysds-feedback-id');
-    if (feedback) {
-        const container = feedback.closest(".o-wysiwyg");
+function fullPage () {
+    var feedbackId = document.getElementById('nysds-feedback-id');
+    if (feedbackId) {
+        const container = feedbackId.closest(".o-wysiwyg");
         container.style.maxWidth = '100%';
         container.style.padding = "0px";
     }
-    else {
-        return;
-    }
-    
+}    
 
-})
+function replaceUrl () {
+    thumbsUp.href = urlUp;
+    thumbsDown.href = urlDown;
+}
+
+document.addEventListener('DOMContentLoaded', fullPage)
+document.addEventListener('DOMContentLoaded', replaceUrl)
