ff9c70b014fe2253c3a9801f862587d7d7a0e8a4
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2024-12-27 10:48:20 -0500

    Start search hero component

---
+++ _includes/searchHero/_searchHero.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('nysds-service-search-input');
+    const searchTerm = searchInput.value; 
+    const submitSearch = document.getElementById('nysds-service-search-submit');
+
+
+    
+
+    console.log(searchTerm)
+})
